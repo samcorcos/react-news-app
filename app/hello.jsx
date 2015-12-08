03a21c64e59e241982b8721b9e62a08251e858ac
Sam Corcos <sam@corcos.io>
2015-12-08 11:54:13 -0800

    intial commit

---
+++ app/hello.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Hello extends Component {
+  render() {
+    return(
+      <h1>Hello, World!</h1>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('app')
+)
